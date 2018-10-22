9c18a2b818a870a73fd214118d53cbca1dd0e85a
Kristin Ottofy <kottofy@outlook.com>
2018-10-22 19:32:08 -0400

    testing

---
+++ ADFARM/ADFARMscript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+STORAGE_KEY=$1
+
+az group deployment create \
+    --resource-group datafactory \
+    --template-file DataFactory/ADFARM/ADFARM.json \
+    --parameters '{
+      "dataFactoryName": {
+        "value": "kodatafactory"
+      },    
+      "dataFactoryLocation": {
+        "value": "East US"
+      },
+      "storageAccountName": {
+        "value": "kodatafactorystorage"
+      },
+      "storageAccountKey": {
+          "value": "'$STORAGE_KEY'"
+      },
+      "blobContainer": {
+        "value": "files"
+      },
+      "inputBlobFolder": {
+        "value": "input"
+      },
+      "inputBlobName": {
+        "value": "blob.txt"
+      },
+      "outputBlobFolder": {
+        "value": "output"
+      },
+      "outputBlobName": {
+        "value": "outputblob.txt"
+      }
+    }'
